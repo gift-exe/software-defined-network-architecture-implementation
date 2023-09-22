5a4b9103abee20aa45c038cad720b838668c5d0a
Gift-py <giftabumere247@gmail.com>
2023-09-22 15:38:25 +0100

    final objective want to suck my blood ooo chai

--- lab/network_data_analysis/custom_packets.py
+++ lab/network_data_analysis/custom_packets.py
@@ -4,3 +4,16 @@
 #ROUND_TRIP_TIME
 #JITTER_AND_PACKET_LOSS
 
+
+'''
+    To measure bandwidth, use the iperf command.
+    Measure bandwith between: h1s1 -- h3s1
+                              h1s1 -- h3s2
+                              h1s1 -- h3s3
+    perform the measurement sequentially and record the bandwidth, 
+    and then perform the measurement at the same time (parallel).
+    you would notice a drop in the bandwidth.
+
+    do the same for the gns3 network.
+
+'''
